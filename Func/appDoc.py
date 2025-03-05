14ce32828fa8b9547f5e584cbe5029ea323aee43
Henry Wang <97229036+HenryBlake@users.noreply.github.com>
2025-03-05 15:09:17 -0500

    Read function achieved

--- Func/appDoc.py
+++ Func/appDoc.py
@@ -1,9 +1,38 @@
 import psutil
 import json
+import time
 class AppDoc:
-    def writeAPP(self,app_list):
-        for name in app_list:
-            return json.dump(app_list)
-    def readApp(self,app_list_doc):
-        for name in app_list_doc:
-            return json.load(app_list_doc)
+    def __init__(self):
+        self.app_time=0
+        self.json_path_name="app_log_doc/app_time_doc.json"
+        self.app_list={}
+
+    def writeAPP(self,app_name):
+        print(f"Writing {app_name}")
+
+    def readApp(self):
+         try: 
+            with open(self.json_path_name,"r",encoding="utf-8") as file:
+                data=json.load(file)
+                return data
+         except FileNotFoundError:
+                print("The file is not found!")
+         except json.JSONDecodeError:
+                print("Decoder ERROR!")
+    
+    def createionTime(self,app_pid):
+        self.app_time= time.time()-psutil.Process(app_pid).create_time()
+        return self.app_time
+    
+    def diffNameCheck(self,app_name):
+         self.app_list=self.readApp()
+        #  for app_name in self.app_list["app_name"]:
+        #     if app_name and app_name.lower() in self.app_list["app_name"].lower():
+        #       print("app exsists!")
+        #     else:
+        #       self.writeAPP(app_name)
+        #  return None
+         if app_name.lower() in self.app_list.values():
+              print("app exsists")
+         else :
+              self.writeAPP(app_name)
--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from PyQt6.QtWidgets import QApplication,QWidget
 import sys
 import asyncio
 import psutil
+import time
 from Func.pidTracker import pidTracker
 from Func.processRuningCheck import processRuningCheck
 from Func.nameTracker import nameTracker
+from Func.appDoc import AppDoc
+
 # app=QApplication(sys.argv)
 # window=QWidget()
 # window.show()
@@ -13,22 +16,34 @@ from Func.nameTracker import nameTracker
 pid_tracker=pidTracker()
 name_tracker=nameTracker()
 runingCheck=processRuningCheck()
+appDoc=AppDoc()
 #Init area ends
 
 
 app_name="steam.exe"
 app_name=input("Please Input Your App'name: ")
+if input("Do you want to save app?").lower() == "y":
+    # print(appDoc.readApp())
+    appDoc.diffNameCheck(app_name)
+else:
+    print("")
 app_pid=pid_tracker.findPID(app_name)
 # runingCheck.process_check(app_pid)
+
+#Test if we can read the list of apps
+# app_list=appDoc.readApp()
+# print(app_list)
+
 async def testFunc(app_pid):
     while True:
         if psutil.pid_exists(app_pid):
-            print(f"{app_name} is runing")
+            print(f"{app_name} is runing, has been runing for {appDoc.createionTime(app_pid)} seconds")
+            # print(time.time() - psutil.Process(app_pid).create_time())
         else:
             print(f"the {app_name} is not runing")
         await asyncio.sleep(5)
-
-asyncio.run(testFunc(app_pid))
+#Start Aysnc
+# asyncio.run(testFunc(app_pid))
 # runingCheck.process_check(app_pid)
 # print(app_pid)
 # for process in psutil.process_iter(attrs=["pid","name"]):
